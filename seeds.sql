bae89e043cc2c467f014ed0a10ba3806859b2df2
Michael Scott <mscotteagle21@gmail.com>
2024-03-17 21:41:44 -0500

    added server.js file with pg connection, completed tables in schema folder, created seed file to insert data.

---
+++ seeds.sql
@@ -0,0 +1,10 @@
+INSERT INTO departments (name, department_id)
+VALUES  ('Finance', 1),
+        ('Engineering', 2),
+        ('Quality', 3); 
+
+
+INSERT INTO roles (title, salary)
+VALUES ('Financial Analyst', 65000),
+        ('Engineer', 85000),
+        ('Quality Engineer', 75000)
