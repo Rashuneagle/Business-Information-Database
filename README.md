135d2d89fb5010b01ed5a4e57b5066041bd6afbe
Michael Scott <mscotteagle21@gmail.com>
2024-04-06 18:55:53 -0500

    finished queries for each operation, sucessfully excuted each prompt to update database, used readMe generator to create readme

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # Business-Information-Database
+
+## Description
+This application can manage your companies employee, departments, and role database. It allows you to view all of each, update each and change your employee role. Also create each.
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+Requires Node.js, Pool to connect to database, and inqurier to prompt user
+
+## Usage
+Usage would be for company database
+
+## License
+![License Badge](https://img.shields.io/badge/license-MIT-brightgreen)
+
+This application is covered under the [MIT license]([MIT License](https://opensource.org/licenses/MIT)).
+
+## Contributing
+Contributors are welcome
+
+## Tests
+N/A
+
+## Questions
+If you have any questions, feel free to reach out to me:
+- GitHub: [Rashuneagle](https://github.com/Rashuneagle)
+- Email: mscotteagle21@gmail.com
--- server.js
+++ server.js
@@ -1,70 +1,155 @@
-const express = require('express');
-
 const inquirer = require('inquirer');
+const db = require('./db/index');
 
-const db = require('./db');
+async function startApp() {
+  try {
+    console.log('Welcome to the Employee Management System!');
 
-const PORT = process.env.PORT || 3002;
-const app = express();
-
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-
-
-
-// Function to perform database query based on user input
-function performDepartmentQuery(tableName) {
-  pool.query(`SELECT * FROM ${tableName}`)
-    .then(result => {
-      console.log(result.rows);
-    })
-    .catch(err => {
-      console.error('Error executing query:', err);
+    const { action } = await inquirer.prompt({
+      type: 'list',
+      name: 'action',
+      message: 'What would you like to do?',
+      choices: [
+        'View all departments',
+        'View all roles',
+        'View all employees',
+        'Add a department',
+        'Add a role',
+        'Add an employee',
+        'Update an employee role',
+        'Exit'
+      ]
     });
+
+    switch (action) {
+      case 'View all departments':
+        await viewAllDepartments();
+        break;
+      case 'View all roles':
+        await viewAllRoles();
+        break;
+      case 'View all employees':
+        await viewAllEmployees();
+        break;
+      case 'Add a department':
+        await addDepartment();
+        break;
+      case 'Add a role':
+        await addRole();
+        break;
+      case 'Add an employee':
+        await addEmployee();
+        break;
+      case 'Update an employee role':
+        await updateEmployeeRole();
+        break;
+      case 'Exit':
+        console.log('Exiting the application.');
+        process.exit();
+      default:
+        console.log('Invalid choice.');
+    }
+  } catch (error) {
+    console.error('Error:', error);
+  }
 }
 
-// questions to prompt the user
-inquirer
-    .prompt([
-        {
-            type: 'list',
-            name: 'table',
-            message: 'Which table would you like to view?',
-            choices: ['departments', 'roles', 'employees']
-        },
-        {
-            type: 'list',
-            name: 'additions',
-            message: 'Would you like to add a department, role or employee?',
-            choices: ['add department', 'add roles', 'add employee']
-        }
-    ])
-    .then(answers => {
-      // Trigger database query based on user input
-      performDepartmentQuery(answers.table);
-    });
+async function viewAllDepartments() {
+  const departments = await db.findAllDepartments();
+  console.table(departments);
+  startApp();
+}
 
-    //.then((res)=>{
-      let choice = res.choice;
-      switch(choice){
-        case 'VIEW_EMPLOYEES':
-          viewEmployees();
-          break;
-      }
+async function viewAllRoles() {
+  const roles = await db.findAllRoles();
+  console.table(roles);
+  startApp();
+}
+
+async function viewAllEmployees() {
+  const employees = await db.findAllEmployees();
+  console.table(employees);
+  startApp();
+}
+
+async function addDepartment() {
+  const { departmentName } = await inquirer.prompt({
+    type: 'input',
+    name: 'departmentName',
+    message: 'Enter the name of the department:'
+  });
+  await db.createDepartment({ name: departmentName });
+  console.log('Department added successfully.');
+  startApp();
+}
+
+async function addRole() {
+  const { roleName, roleSalary, departmentId } = await inquirer.prompt([
+    {
+      type: 'input',
+      name: 'roleName',
+      message: 'Enter the name of the role:'
+    },
+    {
+      type: 'input',
+      name: 'roleSalary',
+      message: 'Enter the Salary for the role:'
+    },
+    {
+      type: 'input',
+      name: 'departmentId',
+      message: 'Enter the department ID for the role:'
     }
-    )
+  ]);
+  await db.createRole({ title: roleName, salary: roleSalary, department_id: departmentId });
+  console.log('Role added successfully.');
+  startApp();
+}
 
-//functions to write: viewEmployees, viewEmployeesByDepartment, viewEmployeesByManager, removeEmployee, updateEmployeeRole, update employee manager, view Roles, add roles, remove roles, viewDepartments, addDepartment, removeDepartments, addEmployee, quit fundtion
+async function addEmployee() {
+  const { firstName, lastName, roleId, managerId } = await inquirer.prompt([
+    {
+      type: 'input',
+      name: 'firstName',
+      message: 'Enter the first name of the employee:'
+    },
+    {
+      type: 'input',
+      name: 'lastName',
+      message: 'Enter the last name of the employee:'
+    },
+    {
+      type: 'input',
+      name: 'roleId',
+      message: 'Enter the role id of the employee:'
+    },
+    {
+      type: 'input',
+      name: 'managerId',
+      message: 'Enter the manager id of the employee:'
+    }
+  ]);
+  await db.createEmployee({ first_name: firstName, last_name: lastName, role_id: roleId, manager_id: managerId });
+  console.log('Employee added successfully.');
+  startApp();
+}
 
-    pool.on('error', err => {
-  console.error('PostgreSQL pool error:', err);
-});
+async function updateEmployeeRole() {
+const { employeeId, roleId } = await inquirer.prompt([
+  {
+    type: 'input',
+    name: 'employeeId',
+    message: 'Enter the ID of the employee:'
+  },
+  {
+    type: 'input',
+    name: 'roleId',
+    message: 'Enter the employees new role ID:'
+  }
+]);
+await db.updateEmployee({ role_id: roleId, id: employeeId});
+console.log('Employee role updated sucessfully.');
+startApp();
+}
 
-// Default response for any other request (Not Found)
-app.use((req, res) => {
-  res.status(404).end();
-});
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+startApp();
